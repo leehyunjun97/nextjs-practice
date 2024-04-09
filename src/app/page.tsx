7496995d3b26fe4a659fcf48dbf84e7a47872769
leehyunjun97 <monsterstamp79@gmail.com>
2024-04-09 20:08:47 +0900

    next-auth 라이브러리 예제

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    // 테일원드의
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl font-semibold'>next-auth 라이브러리 예제</h1>
+    </main>
+  );
+}
